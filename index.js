a5d87d64a35c46d142992bef29c7f1d90281446d
Brianna Dixon <brianna_dixon@rocketmail.com>
2018-12-17 06:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === 'hello!') {
+  if (`${string}` === 'hello!') {
     return "YES INDEED!"
   } 
   else if (string === 'HELLO!') {
