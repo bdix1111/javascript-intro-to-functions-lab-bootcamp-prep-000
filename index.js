412ed97fcc8d06ac36afce24cc5000962f06c872
Brianna Dixon <brianna_dixon@rocketmail.com>
2018-12-17 06:07:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ var uppercase = "HELLO!"
 var lowercase = 'hello!'
 
 function sayHiToGrandma(string) {
-  if (lowercase.toLowerCase() === lowercase) {
+  if (lowercase.toLowerCase(string) === lowercase) {
     return "I can\'t hear you!"
   } 
-  else if (uppercase.toUpperCase() === uppercase) {
+  else if (uppercase.toUpperCase(string) === uppercase) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.")
